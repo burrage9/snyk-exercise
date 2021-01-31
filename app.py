6771dbc0011704741a51a35f0a92680e9ab33d56
Alex Burrage <alexjburrage@gmail.com>
2021-01-31 13:21:52 +0100

    Beginnings of code to parse dependency list, extracting version info, so can query recursively.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import sqlite3
 import secrets
 import requests
+import re
 from flask import Flask, render_template, request, url_for, flash, redirect
 
 def get_db_connection():
@@ -11,15 +12,29 @@ def get_db_connection():
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secrets.secretkey
 
+g_output = '0'
+
 @app.route('/')
 def index():
-    conn = get_db_connection()
-    packages = conn.execute('SELECT * FROM packages').fetchall()
-    conn.close()
+    query = 'SELECT * FROM packages'
+    packages = dbquery(query)
     return render_template('index.html', packages=packages)
     
+@app.route('/results')
+def results():
+    global g_output
+    text = ''.join(('SELECT * FROM packages WHERE id = ',str(g_output),' '))
+    
+    packages = dbquery(text)
+    
+    dep_list = diplayresults(packages)
+
+    return(str(dep_list))
+
+    #return render_template('results.html', packages=packages)
+    
 @app.route('/request', methods=('GET', 'POST'))
-def create():
+def requestform():
     if request.method == 'POST':
         name = request.form['name']
         version = request.form['version']
@@ -35,15 +50,9 @@ def create():
         if not name:
             flash('Name is required!') #should we check for version too?
         else:
-            ret = tablelookup(name, version)
-            if ret == -1:
-                # entry is not in the table, need to request it from the repo
-                remotefetch(name, version)
-                return render_template('request.html')
-            
-            else:
-                # entry is already in the table
-                return redirect(url_for('index'))                   
+            getpackageinfo(name, version)
+
+            return redirect(url_for('results'))
             
     return render_template('request.html')
     
@@ -59,6 +68,7 @@ def tablelookup(name, version):
     if not data:
         return -1
     else:
+        g_output = '23'
         return data[0]['id']
         
 def dbinsert(message, name, version, dependencies):
@@ -73,8 +83,21 @@ def dbquery(message):
     conn.commit()
     conn.close()
     return ret
+    
+def getpackageinfo(name, version):
+    #when this fn ends, g_output is the row in the db of the data.
+    global g_output
+    ret = tablelookup(name, version)
+    if ret == -1:
+        # entry is not in the table, need to request it from the repo
+        remotefetch(name, version)
+    else:
+        # entry is already in the table
+        g_output = ret
+
 
 def remotefetch(name, version):
+    global g_output
     url = ''.join(('https://registry.npmjs.org/',name,'/',version))
     r = requests.get(url)
 
@@ -97,8 +120,49 @@ def remotefetch(name, version):
                 else:
                     dep_list = r.text[start+1:end]
                     dbinsert('INSERT INTO packages (name, version,dependencies) VALUES (?,?,?)',name, version, dep_list)
-                    return redirect(url_for('index'))
+                    
+                    #fetch back the new row id, so it can be displayed
+                    g_output = tablelookup(name, version)
+                    if g_output == -1:
+                        #something has gone wrong, in inserting into the table
+                        return render_template('request.html')
                     
     return render_template('request.html')
     
-   
+def diplayresults(packages):
+    #shouldn't just assume we're in a 1 item array
+    deps = packages[0][4]
+    
+    start = 0
+    end = 1
+    i = 0
+    dep_list = [0] * 100 #max number of deps?
+
+    end = deps.find(",")
+    
+    while end != -1:
+        dep_list[i] = deps[start:end]
+        start = end + 1
+        end = deps.find(",",start)
+        i = i+1
+        
+    i=0
+    while dep_list[i] != 0:
+        start = 0
+        end = 1
+        end = dep_list[i].find(":")
+        name = dep_list[i][2:(end-1)] #need to ignore quote marks
+        
+        version = dep_list[i][end+2:len(dep_list[i])]
+
+        #for now we will just ignore the ~, > etc of the version number, and just find the first number character.
+        m = re.search(r"\d",version)
+
+        version = version[m.start():]
+            
+        getpackageinfo(name, version)
+        #can trust that at this point g_output is the row in the table
+        
+    return dep_list
+    
+    
