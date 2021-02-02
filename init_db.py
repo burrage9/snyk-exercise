891b67b930fbbd7a7237e85600cf2b14db25a5ef
Alex Burrage <alexjburrage@gmail.com>
2021-02-02 21:58:27 +0100

    Tidy up of the committed DB

--- init_db.py
+++ init_db.py
@@ -5,16 +5,5 @@ connection = sqlite3.connect('database.db')
 with open('schema.sql') as f:
   connection.executescript(f.read())
 	
-cur = connection.cursor()
-
-cur.execute("INSERT INTO packages (name, version, dependencies) VALUES (?,?,?)",
-  ('sample',"1.0","sample2")
-  )
-
-cur.execute("INSERT INTO packages (name, version, dependencies) VALUES (?,?,?)",
-  ('sample2',"1.1","other,other2")
-)
-	
 connection.commit()
 connection.close()
-	
